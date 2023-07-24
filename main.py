5ae19e5c67c5a78982b4f3003de5b491bf349712
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-07-24 15:53:16 -0500

    main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ image1 = st.image("LogoSETI.jpeg", caption='SETI',  width=70)
 col1.image(image1, caption='SETI', width=70)
 
 # Imagen 2
-image2 = st.image("LogoAIO.jpeg", caption='SETI',  width=10)
+image2 = st.image("LogoAIO.jpeg", caption='SETI',  width=5)
 col2.image(image2, caption='Otra Imagen', width=70)
 
 st.markdown("<h1 style='color: red;'>Aplicación Estandarización de teléfonos nacionales</h1>", unsafe_allow_html=True)
