8f83ccbf6021871400e84e9a6bb09970981d57e6
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-07-24 15:37:18 -0500

    main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ st.markdown("<h1 style='color: red;'>Aplicación Estandarización de teléfonos 
 
 # Imagen a la derecha en la parte principal
 #image = Image.open("LogoSETI.jpeg")
-st.image("LogoSETI.jpeg", caption='SETI',  width=20)
+st.image("LogoSETI.jpeg", caption='SETI',  width=70)
 #st.image(image, caption='SETI', width=200, use_column_width=False)
 
 # Barra lateral (sidebar)
