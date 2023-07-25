d6614435df7c60b7856b6299e8b9210b31603d92
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-07-25 09:58:38 -0500

    main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ st.markdown(
         color: black; /* Letras negras */
     }
     [data-testid="stSidebar"] {
-        background-color: rgb(255, 255, 255); /* Color de fondo original */
+        background-color: rgb(250, 250, 250); /* Color de fondo original */
         color: #FFFFFF; /* Color de letras original (blanco) */
     }
     [aria-selected="true"] {
