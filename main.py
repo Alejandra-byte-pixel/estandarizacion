1b22adffab9b56e9541360a61d34506949a32d60
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-07-27 19:56:26 -0500

    main.py

--- main.py
+++ main.py
@@ -49,7 +49,8 @@ separador_campos = ';'
 
 # Imagen 1
 #image1 = col1.image("LogoSETI.jpeg", caption='SETI', width=100)
-st.markdown("<p style='text-align: center;'><img src='LogoSETI.jpeg' alt='Logo' width='200'></p>", unsafe_allow_html=True)
+st.image("LogoSETI.jpeg", caption='Logo', use_column_width=True)
+#st.markdown("<p style='text-align: center;'><img src='LogoSETI.jpeg' alt='Logo' width='200'></p>", unsafe_allow_html=True)
 # Espacio entre las imágenes
 #col1.write("")
 
