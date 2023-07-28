29854fdd401be7223d58bdd6a834f4dffd7734a0
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-07-27 20:51:36 -0500

    main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ separador_campos = ';'
 col1, col2 = st.columns([1, 3])  
 
 # Imagen
-col1.image("LogoSetiAio.jpg", caption='Logo', width=500, use_column_width=True)
+col1.image("LogoSetiAio.jpg", caption='Logo', width=100, use_column_width=True)
 
 # Título sin margen superior
 col2.markdown("<h1 style='color: red; margin-top: 0;'>Aplicación Estandarización de teléfonos nacionales</h1>", unsafe_allow_html=True)
