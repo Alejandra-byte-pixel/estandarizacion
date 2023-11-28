132c86b64852a7ab0e1fe7a39238e7162af9f525
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-11-28 09:13:01 -0500

    main_escenario2.py

--- main_escenario2.py
+++ main_escenario2.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import estandarizador_escenario2
 from multiprocessing import Pool
-import time
-inicio = time.time()
+
 
 # Establecer el color de fondo para la parte derecha
 st.markdown(
@@ -18,37 +17,31 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# Estilo para la imagen y el título
+estilo_imagen = """
+    <style>
+    .imagen {
+        display: inline-block;
+        vertical-align: middle;
+    }
+    .titulo {
+        color: red;
+        display: inline-block;
+        vertical-align: middle;
+        margin-left: 10px;
+        margin-top: 0;
+    }
+    </style>
+    """
+st.markdown(estilo_imagen, unsafe_allow_html=True)
 
-def get_diccionario(diccionario: dict):
-    dictionary_depurado: dict = {}
-    if diccionario is not None:
-        for i in diccionario.keys():
-            if diccionario[i] != "":
-                dictionary_depurado[i] = diccionario[i]
-        return dictionary_depurado
-    else:
-        return dictionary_depurado
+# Contenedor principal
+col1, col2 = st.columns([1, 3])  # Definir el ancho de las columnas (en este caso, 1 y 3)
 
-def procesar_direccion(direccion):
-    componentes = estandarizador_escenario2.estandarizar_direccion(direccion)
-    return direccion, get_diccionario(componentes)
+# Imagen
+col1.image("LogoSetiAio.jpg", caption='Logo', width=100, use_column_width=True)
 
-def procesar_archivo(archivo_entrada, archivo_salida):
-    with open(archivo_entrada, 'r') as f:
-        lista_direcciones = f.read().splitlines()
+# Título
+col2.markdown("<h1 class='titulo'>Estandarización de teléfonos nacionales</h1>", unsafe_allow_html=True)
 
-    with Pool(processes=6) as pool:
-        resultados = pool.map(procesar_direccion, lista_direcciones)
 
-    with open(archivo_salida, 'w') as f:
-        for direccion, diccionario in resultados:
-            f.write(f"{direccion}\n{diccionario}\n-------------------------------------------\n")
-
-if __name__ == '__main__':
-    ruta_archivo_entrada = 'C:/Users/aleja/PycharmProjects/Escenario_2_Optimizado/Direcciones_escenario_2.txt'
-    ruta_archivo_salida = 'C:/Users/aleja/PycharmProjects/Escenario_2_Optimizado/Direcciones_estabdarizadas_escenario_2.txt'
-
-    procesar_archivo(ruta_archivo_entrada, ruta_archivo_salida)
-
-fin = time.time()
-print(fin-inicio)
