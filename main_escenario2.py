dbcb25343d306be77d4af3e8ebe387ba9f8859f3
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-11-28 07:42:02 -0500

    Add files via upload

---
+++ main_escenario2.py
@@ -0,0 +1,38 @@
+import estandarizador_escenario2
+from multiprocessing import Pool
+import time
+inicio = time.time()
+
+def get_diccionario(diccionario: dict):
+    dictionary_depurado: dict = {}
+    if diccionario is not None:
+        for i in diccionario.keys():
+            if diccionario[i] != "":
+                dictionary_depurado[i] = diccionario[i]
+        return dictionary_depurado
+    else:
+        return dictionary_depurado
+
+def procesar_direccion(direccion):
+    componentes = estandarizador_escenario2.estandarizar_direccion(direccion)
+    return direccion, get_diccionario(componentes)
+
+def procesar_archivo(archivo_entrada, archivo_salida):
+    with open(archivo_entrada, 'r') as f:
+        lista_direcciones = f.read().splitlines()
+
+    with Pool(processes=6) as pool:
+        resultados = pool.map(procesar_direccion, lista_direcciones)
+
+    with open(archivo_salida, 'w') as f:
+        for direccion, diccionario in resultados:
+            f.write(f"{direccion}\n{diccionario}\n-------------------------------------------\n")
+
+if __name__ == '__main__':
+    ruta_archivo_entrada = 'C:/Users/aleja/PycharmProjects/Escenario_2_Optimizado/Direcciones_escenario_2.txt'
+    ruta_archivo_salida = 'C:/Users/aleja/PycharmProjects/Escenario_2_Optimizado/Direcciones_estabdarizadas_escenario_2.txt'
+
+    procesar_archivo(ruta_archivo_entrada, ruta_archivo_salida)
+
+fin = time.time()
+print(fin-inicio)
