bd7abecb55602c6b1a16305bfddebc0d762b717a
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-11-30 12:11:04 -0500

    LecturaArchivo.py

--- LecturaArchivo.py
+++ LecturaArchivo.py
@@ -1,20 +1,17 @@
 # Opción 1: Utilizando el módulo csv
 import csv
 
-nombre_archivo = "telefonos.csv"  # Reemplaza con el nombre de tu archivo
+nombre_archivo = "telefonos.csv"  
 
 with open(nombre_archivo, "r") as archivo:
     lector_csv = csv.reader(archivo)
     for fila in lector_csv:
         columna = fila[0]
-        # Haz algo con el valor de la columna
         print(columna)
 
 # Opción 2: Abriendo y leyendo el archivo directamente
-nombre_archivo = "telefonos.csv"  # Reemplaza con el nombre de tu archivo
-
+nombre_archivo = "telefonos.csv" 
 with open(nombre_archivo, "r") as archivo:
     for linea in archivo:
         columna = linea.strip()
-        # Haz algo con el valor de la columna
         print(columna)
