d6d6efcee75a293f2dff9092f728fa4ffb895e7c
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-12-01 15:59:16 -0500

    estandarizador_direcciones.py

--- estandarizador_direcciones.py
+++ estandarizador_direcciones.py
@@ -1,29 +1,8 @@
-def procesar_archivo(archivo_entrada):
-    with open(nombre_archivo, 'r') as f:
-        lineas = f.readlines()
-
-    resultados = [procesar_direccion(linea.strip()) for linea in lineas]
-
-    return resultados
-
-if __name__ == '__main__':
-    st.title("Procesador de Direcciones")
-
-      if archivo_entrada is not None:
-        # Procesar el archivo y obtener los resultados
-        resultados = procesar_archivo(archivo_entrada)
-        
-        # Crear un CSV en memoria
-        csv_data = StringIO()
-        csv_writer = csv.writer(csv_data)
-        csv_writer.writerow(["Campo1", "Campo2", "Campo3", "Campo4"])
-        csv_writer.writerows(resultados)
-
-        # Botón de descarga
-        st.download_button(
-            label="Descargar CSV",
-            data=csv_data.getvalue(),
-            file_name="resultados.csv",
-            key="csv_button"
-        )
+def procesar_direccion(cadena):
+    palabras = cadena.split()
+    campo1 = " ".join(palabras)
+    campo2 = " ".join(palabras[:-2])
+    campo3 = palabras[-2]
+    campo4 = "si"
+    return [ADDRESS, ADDR_LINE_ONE, ADDR_LINE_TWO, ADDR_LINE_THREE]
 
