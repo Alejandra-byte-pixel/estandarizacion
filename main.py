8ad2ee48397b362132de03d2d24ba3cbfc5e1e7d
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-12-03 12:09:49 -0500

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from multiprocessing.sharedctypes import Value
 import streamlit as st
 import os
 
+# Establecer el color del texto en la barra lateral y en las opciones
+st.markdown(
+    """
+    <style>
+    .sidebar .sidebar-content {
+        color: white;
+    }
+    .css-145kmo2-singleSelect {
+        color: white;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.set_page_config(layout="wide")
 root = os.path.join(os.path.dirname(__file__))
 
@@ -16,7 +31,6 @@ index = list(dashboards.keys()).index(choice_from_url) if choice_from_url in das
 choice = st.sidebar.radio("Seleccionar Programa", list(dashboards.keys()), index=index)
 
 path = dashboards[choice]
-
 
 with open(path, encoding="utf-8") as code:
     c = code.read()
